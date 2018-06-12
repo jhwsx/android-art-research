21cb1f83e36e24d8bd85c91b64b0c92b4172022b
wangzhichao <392337950@qq.com>
2018-06-13 01:32:04 +0800

    fine tuning

--- chapter_3/src/main/java/com/wzc/chapter_3/slideconflict/view/HorizontalScrollViewEx.java
+++ chapter_3/src/main/java/com/wzc/chapter_3/slideconflict/view/HorizontalScrollViewEx.java
@@ -56,6 +56,12 @@ public class HorizontalScrollViewEx extends ViewGroup {
                 // 参考任玉刚老师代码修改: 在弹性滚动没有结束时, 按下手指, 停止滚动, 立即拦截
                 if (!mScroller.isFinished()) {
                     mScroller.abortAnimation();
+                    // 如果不加下面这句代码, 那么在进行水平滑动时, 还未结束水平滑动松开手指,另一手指就马上进行
+                    // 竖直滑动, 则水平方向的滑动已经被 abort 了, 新的滑动是竖直滑动. 这时,
+                    // 屏幕上的情况是水平方向没有滑到终点, 而竖直方向可以滚动. 见书上 p164页.
+
+                    // 加上这句代码, 那么当水平滑动正在进行时(进入这个 if 语句,就说明水平滑动正在进行),
+                    // 那么直接拦截(即新的点击事件仍然交给父容器处理).
                     intercepted = true;
                 }
                 break;
