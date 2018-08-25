f6257e9f1e46848400f7ff2635991fd5a850d4f8
wangzhichao <wangzhichao@adups.com>
2018-08-25 18:19:35 +0800

    download demo apk

---
+++ chapter_11/src/main/java/com/wzc/chapter_11/AsyncTaskActivity.java
@@ -0,0 +1,10 @@
+package com.wzc.chapter_11;
+
+import android.app.Activity;
+
+/**
+ * @author wzc
+ * @date 2018/8/25
+ */
+public class AsyncTaskActivity extends Activity {
+}
---
+++ chapter_11/src/main/java/com/wzc/chapter_11/CloseUtils.java
@@ -0,0 +1,56 @@
+package com.deep.read.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * 关闭流工具类
+ * @author wzc
+ * @date 2018/7/2
+ */
+public final class CloseUtils {
+
+    private CloseUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * Close the io stream.
+     *
+     * @param closeables closeables
+     */
+    public static void closeIO(final Closeable... closeables) {
+        if (closeables == null) {
+            return;
+        }
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * Close the io stream quietly.
+     *
+     * @param closeables closeables
+     */
+    public static void closeIOQuietly(final Closeable... closeables) {
+        if (closeables == null) {
+            return;
+        }
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException ignored) {
+                    // do nothing
+                }
+            }
+        }
+    }
+}
