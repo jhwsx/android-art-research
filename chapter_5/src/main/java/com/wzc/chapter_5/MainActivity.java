1dd484d254740f2fea037a4a97a62f780643f540
wangzhichao <392337950@qq.com>
2018-08-29 01:49:57 +0800

    chapter_5

--- chapter_5/src/main/java/com/wzc/chapter_5/MainActivity.java
+++ chapter_5/src/main/java/com/wzc/chapter_5/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RemoteViews;
 
 public class MainActivity extends Activity {
 
@@ -32,5 +33,27 @@ public class MainActivity extends Activity {
                 = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(1,notification);
 
+    }
+
+    public void custom_notification(View view) {
+        Notification notification = new Notification();
+        notification.icon = R.mipmap.ic_launcher;
+        notification.tickerText = "hello world";
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        Intent intent = new Intent(MainActivity.this, Demo1Activity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,
+                1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.layout_notification);
+        remoteViews.setTextViewText(R.id.tv_title, "chapter_5");
+        remoteViews.setImageViewResource(R.id.iv, R.drawable.icon);
+        PendingIntent openActivity2PendingIntent = PendingIntent.getActivity(MainActivity.this, 2,
+                new Intent(MainActivity.this, Demo2Activity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.open_activity2, openActivity2PendingIntent);
+        notification.contentView = remoteViews;
+        notification.contentIntent = pendingIntent;
+        NotificationManager manager
+                = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(2,notification);
     }
 }
