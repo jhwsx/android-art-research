1b12884729488a77830cd83cfdc51ebfc8c19a39
wangzhichao <wangzhichao@adups.com>
2018-09-09 20:25:40 +0800

    chapter_5 blog demo

---
+++ chapter_5/src/main/java/com/wzc/chapter_5/DemoBActivity.java
@@ -0,0 +1,16 @@
+package com.wzc.chapter_5;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author wzc
+ * @date 2018/8/31
+ */
+public class DemoBActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_b);
+    }
+}
