49f600e4145fbdd9618c96f9820cf2704c66ee60
wangzhichao <wangzhichao@adups.com>
2018-10-21 19:59:10 +0800

    chapter_8 part

--- chapter_8/src/main/java/com/wzc/chapter_8/MainActivity.java
+++ chapter_8/src/main/java/com/wzc/chapter_8/MainActivity.java
@@ -1,8 +1,12 @@
 package com.wzc.chapter_8;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,5 +18,22 @@ public class MainActivity extends Activity {
 
     public void window(View view) {
         WindowActivity.start(this);
+    }
+
+    public void dialog(View view) {
+        Dialog dialog = new Dialog(getApplicationContext());
+        dialog.setTitle("Title");
+        dialog.setContentView(R.layout.dialog_content);
+        // 使用 getApplicationContext() 的上下文，不加下面这行，会报错：
+        // Dialog: show mWindowManager.addView RuntimeException
+        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ERROR);
+        dialog.show();
+    }
+
+    public void toast(View view) {
+        Toast toast = new Toast(getApplicationContext());
+        toast.setView(LayoutInflater.from(MainActivity.this).inflate(R.layout.toast_content, null));
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.show();
     }
 }
