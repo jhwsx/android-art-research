b655cd46f3ea5d5180635f53f93a066bb73f3c73
wangzhichao <wangzhichao@adups.com>
2018-11-11 20:47:03 +0800

    chapter_3 add layoutParam slide

---
+++ chapter_3/src/main/java/com/wzc/chapter_3/viewslide/DragView4.java
@@ -0,0 +1,45 @@
+package com.wzc.chapter_3.viewslide;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * @author wzc
+ * @date 2018/11/11
+ */
+public class DragView4 extends TextView {
+    public DragView4(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    private int mLastX;
+    private int mLastY;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getRawX();
+        int y = (int) event.getRawY();
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mLastX = x;
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_UP:
+                int offsetX = x - mLastX;
+                int offsetY = y - mLastY;
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) getLayoutParams();
+                params.leftMargin += offsetX;
+                params.topMargin += offsetY;
+                setLayoutParams(params);
+                mLastX = x;
+                mLastY = y;
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+}
