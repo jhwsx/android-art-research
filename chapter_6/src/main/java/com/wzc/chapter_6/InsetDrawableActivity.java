5c44c1982fb5cd5651899bde500724e3433fef24
wangzhichao <willwaywang6@gmail.com>
2020-08-11 21:16:34 +0800

    chapter6: add inset drawable

--- chapter_6/src/main/java/com/wzc/chapter_6/InsetDrawableActivity.java
+++ chapter_6/src/main/java/com/wzc/chapter_6/InsetDrawableActivity.java
@@ -3,6 +3,7 @@ package com.wzc.chapter_6;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.InsetDrawable;
 import android.os.Bundle;
 
 /**
