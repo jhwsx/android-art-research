50a146143d5f7619476252cb5b194770dea9e07c
wangzhichao <willwaywang6@gmail.com>
2021-09-24 20:11:28 +0800

    review chapter_4

--- chapter_4/src/main/java/com/wzc/chapter_4/FlowLayout.java
+++ chapter_4/src/main/java/com/wzc/chapter_4/FlowLayout.java
@@ -22,8 +22,8 @@ public class FlowLayout extends ViewGroup {
 
     private int lineSpacing = 60;
     private int itemHorizontalSpacing = 20;
-    private List<List<View>> allLineViews = new ArrayList<>();
-    private List<Integer> lineHeights = new ArrayList<>();
+//    private List<List<View>> allLineViews = new ArrayList<>();
+//    private List<Integer> lineHeights = new ArrayList<>();
 
     public FlowLayout(Context context) {
         super(context);
@@ -45,82 +45,105 @@ public class FlowLayout extends ViewGroup {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        lineHeights.clear();
-        allLineViews.clear();
-        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+//        lineHeights.clear();
+//        allLineViews.clear();
+//        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+//        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+//        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
         int childCount = getChildCount();
         int lineWidth = 0;
         int lineHeight = 0;
         int totalHeight = 0;
         int selfWidth = 0;
-        List<View> lineViews = new ArrayList<>();
+//        List<View> lineViews = new ArrayList<>();
         for (int i = 0; i < childCount; i++) {
             View child = getChildAt(i);
-            if (child.getVisibility() == View.GONE) {
-                continue;
-            }
-            measureChildView(child, widthMeasureSpec, heightMeasureSpec);
-            int childMeasuredWidth = child.getMeasuredWidth();
-            int childMeasuredHeight = child.getMeasuredHeight();
-            TextView textView = (TextView) child;
-            String text = textView.getText().toString();
-            if (lineWidth + (lineWidth == 0 ? 0 : itemHorizontalSpacing) + childMeasuredWidth + getPaddingLeft() + getPaddingRight() <= widthSize) {
-                // 当前行宽 + 一个 item 的宽度 + 父容器的左右内边距 小于 宽度的参考值，则可以添加到本行
-                lineWidth += (lineWidth == 0 ? 0 : itemHorizontalSpacing) + childMeasuredWidth;
-                lineViews.add(child);
-                lineHeight = Math.max(lineHeight, childMeasuredHeight);
+            if (child.getVisibility() != View.GONE) {
+                measureChildView(child, widthMeasureSpec, heightMeasureSpec);
+                int childMeasuredWidth = child.getMeasuredWidth();
+                int childMeasuredHeight = child.getMeasuredHeight();
+                TextView textView = (TextView) child;
+                String text = textView.getText().toString();
+                if (lineWidth + (lineWidth == 0 ? 0 : itemHorizontalSpacing) + childMeasuredWidth + getPaddingLeft() + getPaddingRight() <= widthSize) {
+                    // 当前行宽 + 一个 item 的宽度 + 父容器的左右内边距 小于 宽度的参考值，则可以添加到本行
+                    lineWidth += (lineWidth == 0 ? 0 : itemHorizontalSpacing) + childMeasuredWidth;
+//                    lineViews.add(child);
+                    lineHeight = Math.max(lineHeight, childMeasuredHeight);
 //                Log.d(TAG, "onMeasure: lineNum=" + allLineViews.size() + ":" + text);
-            } else {
-                // 换行了
-                // 计算最大行宽
-                selfWidth = Math.max(lineWidth, selfWidth);
-                Log.d(TAG, "onMeasure: lineNum=" + allLineViews.size() + ":" + lineWidth );
-                allLineViews.add(lineViews);
-                lineHeights.add(lineHeight);
-                // 计算总高度
-                totalHeight += lineHeight;
-                totalHeight += lineSpacing;
-                // 清空一行内包含元素的列表
-                lineViews = new ArrayList<>();
-                // 新的一行开始了，把这个元素添加到新的一行里面
-                lineWidth = childMeasuredWidth;
-                lineViews.add(child);
-                lineHeight = childMeasuredHeight;
+                } else {
+                    // 换行了
+                    // 计算最大行宽
+                    selfWidth = Math.max(lineWidth, selfWidth);
+//                    Log.d(TAG, "onMeasure: lineNum=" + allLineViews.size() + ":" + lineWidth);
+//                    allLineViews.add(lineViews);
+//                    lineHeights.add(lineHeight);
+                    // 计算总高度
+                    totalHeight += lineHeight;
+                    totalHeight += lineSpacing;
+                    // 清空一行内包含元素的列表
+//                    lineViews = new ArrayList<>();
+                    // 新的一行开始了，把这个元素添加到新的一行里面
+                    lineWidth = childMeasuredWidth;
+//                    lineViews.add(child);
+                    lineHeight = childMeasuredHeight;
 //                Log.d(TAG, "onMeasure: lineNum=" + allLineViews.size() + ":" + text );
+                }
             }
+
             if (i == childCount - 1) {
                 selfWidth = Math.max(lineWidth, selfWidth);
-                Log.d(TAG, "onMeasure: lineNum=" + allLineViews.size() + ":" + lineWidth );
-                allLineViews.add(lineViews);
-                lineHeights.add(lineHeight);
+//                Log.d(TAG, "onMeasure: lineNum=" + allLineViews.size() + ":" + lineWidth);
+//                allLineViews.add(lineViews);
+//                lineHeights.add(lineHeight);
                 totalHeight += lineHeight;
             }
         }
         Log.d(TAG, "onMeasure: selfWidth=" + selfWidth);
         totalHeight += getPaddingTop() + getPaddingBottom();
         selfWidth += getPaddingLeft() + getPaddingRight();
-        int finalWidth = widthMode == MeasureSpec.EXACTLY ? widthSize : selfWidth;
-        int finalHeight = heightMode == MeasureSpec.EXACTLY ? heightSize : totalHeight;
+
+        int finalWidth = resolveSize(selfWidth, widthMeasureSpec);
+        int finalHeight = resolveSize(totalHeight, heightMeasureSpec);
         setMeasuredDimension(finalWidth, finalHeight);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+//        int childLeft = getPaddingLeft();
+//        int childTop = getPaddingTop();
+//        for (int i = 0; i < allLineViews.size(); i++) {
+//            List<View> lineViews = allLineViews.get(i);
+//            for (int j = 0; j < lineViews.size(); j++) {
+//                View view = lineViews.get(j);
+//                view.layout(childLeft, childTop, childLeft + view.getMeasuredWidth(), childTop + view.getMeasuredHeight());
+//                childLeft += view.getMeasuredWidth() + itemHorizontalSpacing;
+//            }
+//            childLeft = getPaddingLeft();
+//            childTop += lineHeights.get(i) + lineSpacing;
+//        }
         int childLeft = getPaddingLeft();
         int childTop = getPaddingTop();
-        for (int i = 0; i < allLineViews.size(); i++) {
-            List<View> lineViews = allLineViews.get(i);
-            for (int j = 0; j < lineViews.size(); j++) {
-                View view = lineViews.get(j);
-                view.layout(childLeft, childTop, childLeft + view.getMeasuredWidth(), childTop + view.getMeasuredHeight());
-                childLeft += view.getMeasuredWidth() + itemHorizontalSpacing;
+        int childCount = getChildCount();
+//        int lineCount = 1;
+        int lineHeight = 0;
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            int childMeasuredWidth = child.getMeasuredWidth();
+            int childMeasuredHeight = child.getMeasuredHeight();
+            if (child.getVisibility() != View.GONE) {
+                if (childLeft + childMeasuredWidth > getMeasuredWidth()) {
+                    // 需要另起一行了
+                    childLeft = getPaddingLeft();
+                    childTop += lineHeight + lineSpacing;
+//                    lineCount++;
+                    lineHeight = 0;
+                }
+                child.layout(childLeft, childTop, childLeft + childMeasuredWidth, childTop + childMeasuredHeight);
+                childLeft += child.getMeasuredWidth() + itemHorizontalSpacing;
+                lineHeight = Math.max(lineHeight, childMeasuredHeight);
             }
-            childLeft = getPaddingLeft();
-            childTop += lineHeights.get(i) + lineSpacing;
         }
     }
 
