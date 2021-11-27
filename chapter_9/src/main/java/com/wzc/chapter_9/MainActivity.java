f784a61014f6d003cf7de2d6dc65a520f01b38bf
wangzhichao <willwaywang6@gmail.com>
2021-11-27 20:11:18 +0800

    chapter_9

--- chapter_9/src/main/java/com/wzc/chapter_9/MainActivity.java
+++ chapter_9/src/main/java/com/wzc/chapter_9/MainActivity.java
@@ -1,8 +1,13 @@
 package com.wzc.chapter_9;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +15,32 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent();
-        getApplicationContext().startActivity(intent);
+    }
+
+    public void openWindow(View view) {
+        WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        View v = getLayoutInflater().inflate(R.layout.window_layout, null);
+        v.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.start(v.getContext());
+            }
+        });
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.WRAP_CONTENT,
+                0,0, PixelFormat.TRANSPARENT);
+        layoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        layoutParams.gravity = Gravity.CENTER;
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+        if (windowManager != null) {
+            windowManager.addView(v, layoutParams);
+        }
+    }
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, MainActivity.class);
+        starter.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(starter);
     }
 }
