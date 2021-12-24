ab14b99a209678a3ac52bbb670f4e75e0c789bd7
wangzhichao <willwaywang6@gmail.com>
2021-12-24 09:54:44 +0800

    chapter_9 review

--- chapter_9/src/main/java/com/wzc/chapter_9/MyDynamicReceiver.java
+++ chapter_9/src/main/java/com/wzc/chapter_9/MyDynamicReceiver.java
@@ -8,11 +8,13 @@ import android.util.Log;
 public class MyDynamicReceiver extends BroadcastReceiver {
     private static final String TAG = "MyDynamicReceiver";
     public static final String ACTION_LAUNCH_DYNAMIC = "com.wzc.receiver.LAUNCH_DYNAMIC";
-
+    public int i = 0;
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         Log.d(TAG, "onReceive: receive action = " + action);
+        i++;
+        Log.d(TAG, "onReceive: i = " + i);
         if (ACTION_LAUNCH_DYNAMIC.equals(action)) {
             Log.d(TAG, "onReceive: do launch work...");
         }
