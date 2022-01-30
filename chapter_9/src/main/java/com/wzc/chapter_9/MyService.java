806a6b4eba543b013d81032ca69f03a522ed32e9
wangzhichao <willwaywang6@gmail.com>
2022-01-30 11:39:30 +0800

    chapter_11 review

--- chapter_9/src/main/java/com/wzc/chapter_9/MyService.java
+++ chapter_9/src/main/java/com/wzc/chapter_9/MyService.java
@@ -3,8 +3,11 @@ package com.wzc.chapter_9;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.util.Log;
-
+/*
+TODO https://www.cnblogs.com/jzssuanfa/p/6758379.html
+ */
 public class MyService extends Service {
 
     private static final String TAG = "MyService";
@@ -25,6 +28,9 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        stopSelf();
+//        stopService(intent);
+        SystemClock.sleep(3000);
         Log.d(TAG, "onStartCommand: intent=" + intent + ", flags=" + flags + ", startId=" + startId);
         return super.onStartCommand(intent, flags, startId);
     }
